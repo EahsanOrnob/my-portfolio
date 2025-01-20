3c068a81853c558503064082efd17fb0f4c3bfda
Jamal Mostafa Eahsan <jamalmostafaeahsan@gmail.com>
2025-01-20 17:24:57 +0600

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
->>>>>>> 20164b3e2762eafd1584f03029ccec6bd226be7b
